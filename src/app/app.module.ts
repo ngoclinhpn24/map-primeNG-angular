6e1805215aa5e3e142dcc503fef875d78241370e
Linh Pham <linhpn2402@gmail.com>
2021-07-13 17:20:23 +0700

    shape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {GMapModule} from 'primeng/gmap';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    GMapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
